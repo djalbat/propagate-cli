4f0aeec976a10d43e6961b1a7dc1ded3fc72facd
James Smith <james.smith@djalbat.com>
2020-05-05 09:28:37 +0100

    Tiny cosmetic changes.

--- bin/abbreviations.js
+++ bin/abbreviations.js
@@ -5,9 +5,9 @@ const options = require("./options");
 const { HELP_OPTION, FORCE_OPTION, VERSION_OPTION, QUIETLY_OPTION, DRY_RUN_OPTION } = options;
 
 module.exports = {
-  "h" : HELP_OPTION,
-  "f" : FORCE_OPTION,
-  "v" : VERSION_OPTION,
-  "q" : QUIETLY_OPTION,
-  "d" : DRY_RUN_OPTION
+  "h": HELP_OPTION,
+  "f": FORCE_OPTION,
+  "v": VERSION_OPTION,
+  "q": QUIETLY_OPTION,
+  "d": DRY_RUN_OPTION
 };
