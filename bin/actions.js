d9b481169cc9a7d9cf3fd9569818ac92437b1dbd
James Smith <james.smith@djalbat.com>
2022-11-11 10:08:56 +0000

    Cosmetic changes.

--- bin/actions.js
+++ bin/actions.js
@@ -41,10 +41,10 @@ function actions(command, argument, options) {
 
   if (false) {
     ///
-  } else if (version) {
-    command = VERSION_COMMAND;
   } else if (help) {
     command = HELP_COMMAND;
+  } else if (version) {
+    command = VERSION_COMMAND;
   } else if (commandMissing) {
     command = HELP_COMMAND;
   }
