c49fe3378b20d7c12149deeba79964c78957de88
James Smith <james.smith@djalbat.com>
2022-11-11 11:32:01 +0000

    Changes around the default actions.

--- bin/main.js
+++ bin/main.js
@@ -5,31 +5,33 @@ const { pathUtilities } = require("necessary");
 const actions = require("./actions");
 
 const { TWO_DOTS } = require("./constants"),
-      { PROPAGATE_COMMAND } = require("./commands"),
+      { PROPAGATE_COMMAND } = require("./commands");
+const { DEFAULT_HELP, DEFAULT_VERSION } = require("./defaults"),
       { checkConfigurationFileExists, migrateConfigurationFile } = require("./configuration");
 
 const { bottommostNameFromPath } = pathUtilities;
 
 function main(command, argument, options) {
+  const commandExists = (command !== null),
+        { help = DEFAULT_HELP, version = DEFAULT_VERSION } = options;
+
   let configurationFileExists = checkConfigurationFileExists();
 
-  if (command === null) {
-    if (!configurationFileExists) {
-      const currentWorkingDirectoryPath = process.cwd(); ///
+  if (!help && !version && !commandExists && !configurationFileExists) {  ///
+    const currentWorkingDirectoryPath = process.cwd(); ///
 
-      process.chdir(TWO_DOTS);
+    process.chdir(TWO_DOTS);
 
-      const oldCurrentWorkingDirectoryPath = currentWorkingDirectoryPath; ///
+    const oldCurrentWorkingDirectoryPath = currentWorkingDirectoryPath; ///
 
-      configurationFileExists = checkConfigurationFileExists();
+    configurationFileExists = checkConfigurationFileExists();
 
-      if (configurationFileExists) {
-        const bottommostOldCurrentWorkingDirectoryName = bottommostNameFromPath(oldCurrentWorkingDirectoryPath);
+    if (configurationFileExists) {
+      const bottommostOldCurrentWorkingDirectoryName = bottommostNameFromPath(oldCurrentWorkingDirectoryPath);
 
-        command = PROPAGATE_COMMAND;  ///
+      command = PROPAGATE_COMMAND;  ///
 
-        argument = bottommostOldCurrentWorkingDirectoryName; ///
-      }
+      argument = bottommostOldCurrentWorkingDirectoryName; ///
     }
   }
 
