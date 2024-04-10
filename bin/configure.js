b3d71d20a511f0f6be5b7b80fdbfd55857484db4
James Smith <james.smith@djalbat.com>
2024-04-10 19:42:00 +0100

    Done away with the use of the main file.

---
+++ bin/configure.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const { pathUtilities } = require("necessary");
+
+const { TWO_DOTS } = require("./constants"),
+      { PROPAGATE_COMMAND } = require("./commands"),
+      { DEFAULT_HELP, DEFAULT_VERSION } = require("./defaults"),
+      { checkConfigurationFileExists, migrateConfigurationFile } = require("./configuration");
+
+const { bottommostNameFromPath } = pathUtilities;
+
+function configure(command, argument, options, callback) {
+  let configurationFileExists = checkConfigurationFileExists();
+
+  if (!configurationFileExists) {
+    const { help = DEFAULT_HELP, version = DEFAULT_VERSION } = options;
+
+    if ((help === false) && (version === false) && (command === null)) {
+      const currentWorkingDirectoryPath = process.cwd(); ///
+
+      process.chdir(TWO_DOTS);
+
+      const oldCurrentWorkingDirectoryPath = currentWorkingDirectoryPath; ///
+
+      configurationFileExists = checkConfigurationFileExists();
+
+      if (configurationFileExists) {
+        const bottommostOldCurrentWorkingDirectoryName = bottommostNameFromPath(oldCurrentWorkingDirectoryPath);
+
+        command = PROPAGATE_COMMAND;  ///
+
+        argument = bottommostOldCurrentWorkingDirectoryName; ///
+      }
+    }
+  }
+
+  if (configurationFileExists) {
+    migrateConfigurationFile();
+  }
+
+  callback(command, argument, options);
+}
+
+module.exports = configure;
